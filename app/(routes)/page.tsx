1d755aaea651d2244d229a8669ab325474e50929
Hatem902 <hatelm902@gmail.com>
2023-09-14 00:38:29 +0100

    fix: fixed 'featured articles' billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,27 +1,25 @@
-import getBillboard from "@/actions/get-billboard";
-import getProducts from "@/actions/get-products";
-import ProductList from "@/components/product-list";
-import Billboard from "@/components/ui/billboard";
-import Container from "@/components/ui/container";
+import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
+import ProductList from '@/components/product-list';
+import Billboard from '@/components/ui/billboard';
+import Container from '@/components/ui/container';
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard('224ce266-415e-4a3b-b813-12809a93b8de');
 
   return (
     <Container>
-      <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
-        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Featured Products" items={products} />
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+          <ProductList title='Featured Products' items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,37 +1,35 @@
-"use client";
+'use client';
 
-import NextImage from "next/image";
-import { Tab } from "@headlessui/react";
+import NextImage from 'next/image';
+import { Tab } from '@headlessui/react';
 
-import { Image } from "@/types";
+import { Image } from '@/types';
 
-import GalleryTab from "./gallery-tab";
+import GalleryTab from './gallery-tab';
 
 interface GalleryProps {
   images: Image[];
 }
 
-const Gallery: React.FC<GalleryProps> = ({
-  images = []
-}) => {
-  return ( 
-    <Tab.Group as="div" className="flex flex-col-reverse">
-      <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-        <Tab.List className="grid grid-cols-4 gap-6">
+const Gallery: React.FC<GalleryProps> = ({ images = [] }) => {
+  return (
+    <Tab.Group as='div' className='flex flex-col-reverse'>
+      <div className='mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none'>
+        <Tab.List className='grid grid-cols-4 gap-6'>
           {images.map((image) => (
             <GalleryTab key={image.id} image={image} />
           ))}
         </Tab.List>
       </div>
-      <Tab.Panels className="aspect-square w-full">
+      <Tab.Panels className='aspect-square w-full'>
         {images.map((image) => (
           <Tab.Panel key={image.id}>
-            <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+            <div className='aspect-square relative h-full w-full sm:rounded-lg overflow-hidden'>
               <NextImage
                 fill
                 src={image.url}
-                alt="Image"
-                className="object-cover object-center"
+                alt='Image'
+                className='object-cover object-center'
               />
             </div>
           </Tab.Panel>
@@ -39,6 +37,6 @@ const Gallery: React.FC<GalleryProps> = ({
       </Tab.Panels>
     </Tab.Group>
   );
-}
- 
+};
+
 export default Gallery;
